15ca7e9c61631418c68c46c0eef41ddf6c0c1ea8
Corinne Seckinger <cseckinger0525@gmail.com>
2020-09-16 20:33:45 -0500

    finished exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.print.DocFlavor;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+//@ResponseBody
+//@RequestMapping("hello")
 public class HelloController {
 
     //Handles requests at /hello
@@ -18,42 +18,70 @@ public class HelloController {
 //    }
 
     //now lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
-
-    //create a Handler that handles requests as a form /Hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, "+ name + "!";
-    }
+//    @GetMapping("goodbye")
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+//    }
+//
+//    //create a Handler that handles requests as a form /Hello?name=LaunchCode
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
 
     //handles a request of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam (@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
+//    @GetMapping("{name}")
+//    public String helloWithPathParam(@PathVariable String name) {
+//        return "Hello, " + name + "!";
+//    }
 
-    //lives at /hello/form
+    //lives at /form
     @GetMapping("form")
+    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action = 'hello' method = 'post'>" +
+                "<form action = 'hello' method = 'POST'>" +
                 "<input type = 'text' name = 'name' />" +
-                "<select name=\"languages\" id=\"language-select\">" +
-                "    <option value=\"\">--Please choose a language--</option>\n" +
-                "    <option value=\"english\">English</option>\n" + //hello
-                "    <option value=\"french\">French</option>\n" + //bonjour
-                "    <option value=\"german\">German</option>\n" + //guten tag
-                "    <option value=\"italian\">Italian</option>\n" + //ciao
-                "    <option value=\"spanish\">Spanish</option>\n" + //hola
-                "    <option value=\"hungarian\">Hungarian</option>\n" + //szia
+                "<select name='languages' id=language-select>" +
+                "    <option value='choice'>Please choose a language</option>" +
+                "    <option value='english'>English</option>" + //hello
+                "    <option value='french'>French</option>" + //bonjour
+                "    <option value='german'>German</option>" + //guten tag
+                "    <option value='italian'>Italian</option>" + //ciao
+                "    <option value='spanish'>Spanish</option>" + //hola
+                "    <option value='hungarian'>Hungarian</option>" + //szia
                 "</select>" +
                 "<input type = 'submit' value = 'Greet Me!' />" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+
+    @PostMapping("hello")
+    @ResponseBody
+    public String sayHello(@RequestParam String name, @RequestParam String languages) {
+        return createMessage(name, languages);
+    }
+
+    public static String createMessage(String name, String languages) {
+        String result = "";
+        if (languages.equals("english")) {
+            result = "Hello " + name;
+        } else if (languages.equals("french")) {
+            result = "Bonjour " + name;
+        } else if (languages.equals("german")) {
+            result = "Guten Tag " + name;
+        } else if (languages.equals("italian")) {
+            result = "Ciao " + name;
+        } else if (languages.equals("spanish")) {
+            result = "Hola " + name;
+        } else if (languages.equals("hungarian")) {
+            result = "Szia " + name;
+        } if (languages.equals("choices")) {
+            result = name + "Pick a language";
+        }
+        return result;
+    }
+
 }
